348d33d467565d2e238495cb83909dcf5c03d629
Vladusick <samuelfs@mail.ru>
2024-02-20 10:03:25 +0400

    added search error handler

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,9 +1,15 @@
-import { Movie } from "./Movie"
+import { Movie } from "./Movie";
 
 export function Movies(props) {
-  const { movies } = props
+  const { movies = [] } = props;
 
-  return <div className="movies">
-    {movies.map(movie => <Movie key={movie.imdbID} {...movie} />)}
-  </div>
+  return (
+    <div className="movies">
+      {movies.length ? (
+        movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
+      ) : (
+        <h4>Nothing found</h4>
+      )}
+    </div>
+  );
 }
