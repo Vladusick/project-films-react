bec121946e8382fb4d6828e1a96ce8c4d7ed91b5
Vladusick <samuelfs@mail.ru>
2024-03-28 19:27:15 +0400

    added new link in header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,14 +2,22 @@ function Header() {
   return (
     <nav className="deep-purple darken-2">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
-          Movies
-        </a>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li>
-            <a href="https://github.com/Vladusick/project-films-react">Repo</a>
-          </li>
-        </ul>
+        <div className="row">
+          <div className="col s6">
+            <a href="#" className="brand-logo">
+              Movies
+            </a>
+          </div>
+          <div className="col s6" style={{ textAlign: "right" }}>
+            <a
+              href="https://github.com/Vladusick/project-films-react"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Repo
+            </a>
+          </div>
+        </div>
       </div>
     </nav>
   );
